395062d9b1511af0b24cb330ef87927ce036c539
Gabriel Bogo <gabrielmedeirosbogo@gmail.com>
2019-05-28 23:58:08 -0700

    eval_model returning parameters, added StratKfold and printing summary in the end

--- pipeline/src/build_model.py
+++ pipeline/src/build_model.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed May  8 11:55:39 2019
-
-@author: Carrie Cheung
-
-This step trains an initial model using the provided training dataset.
-"""
 
 import pandas as pd
 import numpy as np
@@ -19,48 +12,67 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import classification_report, accuracy_score, f1_score, confusion_matrix
-from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import cross_val_predict, StratifiedKFold
 from joblib import dump, load
+from time import time
 
 # makefile command
 # python build_model.py non_telem_features.csv rock_class model_results.txt
 
-#### MAIN
-# First check if command line arguments are provided before launching main script
-if len(sys.argv) == 4:
-    train_path = sys.argv[1]
-    powder_path = sys.argv[2]
-    results_path = sys.argv[3]
+train_path = sys.argv[1]
+powder_path = sys.argv[2]
+results_path = sys.argv[3]
 
-    # Read train dataset from files
-    df = pd.read_csv(train_path, low_memory=False)
-    print('Training data dimensions:\n', df.shape)
+# Read train dataset from files
+df = pd.read_csv(train_path, low_memory=False)
+print('Training data dimensions:\n', df.shape)
 
-    # Filter out holes/rows which have no target label
-    df = df.dropna(subset = ["litho_rock_class"])
+# Filter out holes/rows which have no target label
+df = df.dropna(subset = ["litho_rock_class"])
 
-    # Exclude columns from features
-    cols_to_exclude = ["hole_id", "exp_rock_type", "exp_rock_class", "litho_rock_type", "litho_rock_class", 'ActualX_mean', 'ActualY_mean']
+# Exclude columns from features
+cols_to_exclude = ["hole_id", "exp_rock_type", "exp_rock_class", "litho_rock_type", "litho_rock_class", 'ActualX_mean', 'ActualY_mean']
 
-    # Separate target and features
-    X = df.drop(columns=cols_to_exclude)
-    y = df.litho_rock_class # Target column
+# Separate target and features
+X = df.drop(columns=cols_to_exclude)
+y = df.litho_rock_class # Target column
 
-    # Load explosive density data
-    df_exp = pd.read_csv(powder_path)
-    cost_dict = dict(zip(df_exp.rock_class, df_exp["kg/m3"]))
+# K-fold strategy
+kfold = StratifiedKFold(10, random_state=123)
 
-    # Simple random forest model to test evaluate function
-    clf = RandomForestClassifier(n_estimators=430, random_state=0)
-    y_pred = cross_val_predict(clf, X, y, cv=8)
-    overall_cost = calc_overall_cost(y, y_pred, cost_dict)
-    model_name = "Basic model"
-    print_model_eval(y, y_pred, model_name, overall_cost)
+# Load explosive density data
+df_exp = pd.read_csv(powder_path)
+cost_dict = dict(zip(df_exp.rock_class, df_exp["kg/m3"]))
 
-    # Simple random forest model after grouping QZ & LIM
-    y_grouped = pd.Series(np.where(np.logical_or(y == "LIM", y == "QZ"), "LIM", y), name="litho_rock_class")
-    clf = RandomForestClassifier(n_estimators=430, random_state=0)
-    y_pred = cross_val_predict(clf, X, y_grouped, cv=8)
-    model_name = "Basic model after grouping QZ & LIM"
-    overall_cost = calc_overall_cost(y_grouped, y_pred, cost_dict)
-    print_model_eval(y_grouped, y_pred, model_name, overall_cost)
+# Simple random forest model to test evaluate function
+clf = RandomForestClassifier(n_estimators=430, random_state=0)
+tic = time()
+y_pred_m1 = cross_val_predict(clf, X, y, cv=kfold)
+toc = time()
+name_m1 = "Basic model"
+acc_m1, f1_m1, time_m1, cost_m1 = print_model_eval(y,
+                                                        y_pred_m1,
+                                                        name_m1,
+                                                        (toc-tic), cost_dict)
+
+# Simple random forest model after grouping QZ & LIM
+y_grouped = pd.Series(np.where(np.logical_or(y == "LIM", y == "QZ"), "LIM", y), name="litho_rock_class")
+clf = RandomForestClassifier(n_estimators=430, random_state=0)
+tic = time()
+y_pred_m2 = cross_val_predict(clf, X, y_grouped, cv=kfold)
+toc = time()
+name_m2 = "Basic model after grouping QZ & LIM"
+acc_m2, f1_m2, time_m2, cost_m2 = print_model_eval(y_grouped,
+                                                        y_pred_m2,
+                                                        name_m2,
+                                                        (toc-tic), cost_dict)
+
+df_summary = pd.DataFrame(data={
+    "Model Description": [name_m1, name_m2],
+    "Accuracy": [acc_m1, acc_m2],
+    "Macro F1": [f1_m1, f1_m2],
+    "Evaluation Time": [time_m1, time_m2],
+    "Absoltute Explosive Diff": [cost_m1, cost_m2]
+})
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    print(df_summary)
--- pipeline/src/helpers/model.py
+++ pipeline/src/helpers/model.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
 from sklearn.metrics import classification_report, accuracy_score, f1_score, confusion_matrix
-from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import cross_val_predict
 import warnings
 
-def print_model_eval(y_true, y_pred, model_name, overall_cost):
+def print_model_eval(y_true, y_pred, model_name, eval_time, cost_dict):
     unique_values = np.unique(y_true)
     print("\n" + model_name + ":")
+    print("Evaluated in %.2f s" % eval_time)
     confus = confusion_matrix(y_true, y_pred, labels=unique_values)
     #Correct headers
     confus_ex = pd.DataFrame(confus,
@@ -18,8 +18,15 @@ def print_model_eval(y_true, y_pred, model_name, overall_cost):
     print('CLASSIFICATION REPORT:')
     with warnings.catch_warnings():
         warnings.filterwarnings('ignore')
+        f1 = f1_score(y_true, y_pred, average="macro")
         print(classification_report(y_true, y_pred, target_names=unique_values))
+    overall_cost = calc_overall_cost(y_true, y_pred, cost_dict)
     print("Sum of explosive misclassifications: %.3f kg/m3" % overall_cost)
+
+    acc = accuracy_score(y_true, y_pred)
+
+
+    return acc, f1, eval_time, overall_cost
 
 def calc_overall_cost(y_true, y_pred, cost_dict):
     if len(y_pred) != len(y_true):
