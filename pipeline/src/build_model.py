2ce77867fb9bb2e3dd8c9cb9ffb27b6bb9203d13
Gabriel Bogo <gabrielmedeirosbogo@gmail.com>
2019-05-28 22:07:39 -0700

    tried model that grouped LIM and QZ - results are not good

--- pipeline/src/build_model.py
+++ pipeline/src/build_model.py
@@ -13,15 +13,13 @@ import numpy as np
 import re
 import sys
 import pickle
-from helpers.model import evaluate
+from helpers.model import calc_overall_cost, print_model_eval
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import f1_score
+from sklearn.metrics import classification_report, accuracy_score, f1_score, confusion_matrix
+from sklearn.model_selection import cross_val_predict
 from joblib import dump, load
 
 # makefile command
@@ -36,7 +34,7 @@ if len(sys.argv) == 4:
 
     # Read train dataset from files
     df = pd.read_csv(train_path, low_memory=False)
-    print('Training data dimensions:', df.shape)
+    print('Training data dimensions:\n', df.shape)
 
     # Filter out holes/rows which have no target label
     df = df.dropna(subset = ["litho_rock_class"])
@@ -48,20 +46,21 @@ if len(sys.argv) == 4:
     X = df.drop(columns=cols_to_exclude)
     y = df.litho_rock_class # Target column
 
-    ###### PLACE MODEL(S) HERE
+    # Load explosive density data
+    df_exp = pd.read_csv(powder_path)
+    cost_dict = dict(zip(df_exp.rock_class, df_exp["kg/m3"]))
+
     # Simple random forest model to test evaluate function
-    clf = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
-    clf.fit(X, y)
-    # Saves model evaluation results to a text file
-    with open(results_path, "w") as outfile:
-        evaluate(clf, 'random forest', X, y, powder_path, outfile, cv_folds=8)
+    clf = RandomForestClassifier(n_estimators=430, random_state=0)
+    y_pred = cross_val_predict(clf, X, y, cv=8)
+    overall_cost = calc_overall_cost(y, y_pred, cost_dict)
+    model_name = "Basic model"
+    print_model_eval(y, y_pred, model_name, overall_cost)
 
     # Simple random forest model after grouping QZ & LIM
-    y_grouped = pd.Series(np.where(np.logical_or(y == "LIM", y == "QZ"), "LIMQZ", y), name="litho_rock_class")
-    clf2 = RandomForestClassifier(n_estimators=430, random_state=0)
-    clf2.fit(X, y_grouped)
-    results_path2 = "doc/qzlim_grouped_model_results.txt"
-    with open(results_path2, "w") as outfile:
-        evaluate(clf2, 'random forest grouping qz and lim', X, y_grouped, powder_path, outfile, cv_folds=8)
-
-    #dump(clf, 'random_forest_model.pkl')
+    y_grouped = pd.Series(np.where(np.logical_or(y == "LIM", y == "QZ"), "LIM", y), name="litho_rock_class")
+    clf = RandomForestClassifier(n_estimators=430, random_state=0)
+    y_pred = cross_val_predict(clf, X, y_grouped, cv=8)
+    model_name = "Basic model after grouping QZ & LIM"
+    overall_cost = calc_overall_cost(y_grouped, y_pred, cost_dict)
+    print_model_eval(y_grouped, y_pred, model_name, overall_cost)
