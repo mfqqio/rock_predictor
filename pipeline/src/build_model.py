4c558ac3b062406920b4cf36e2b2cd2c533b7599
Gabriel Bogo <gabrielmedeirosbogo@gmail.com>
2019-05-29 21:48:05 -0700

    checkpoint may 29, pipeline running ok

--- pipeline/src/build_model.py
+++ pipeline/src/build_model.py
@@ -46,94 +46,123 @@ kfold = StratifiedKFold(10, shuffle=True, random_state=123)
 df_exp = pd.read_csv(powder_path)
 cost_dict = dict(zip(df_exp.rock_class, df_exp["kg/m3"]))
 
+# Lists of summary metrics
+names = []
+accs = []
+f1s = []
+times = []
+costs = []
+lists = [names, accs, f1s, times, costs]
+
 # Exploration model
-y_pred_m0 = df.exp_rock_class
-name_m0 = "Exploration model"
-acc_m0, f1_m0, time_m0, cost_m0 = evaluate_model(y,
-                                                 y_pred_m0,
-                                                 name_m0,
-                                                 0, cost_dict)
+y_pred = df.exp_rock_class
+appenders = evaluate_model(y, y_pred,"Exploration model", 0, cost_dict)
+[lst.append(x) for lst, x in zip(lists, appenders)]
 
 # Simple random forest model to test evaluate function
 clf = RandomForestClassifier(n_estimators=430, random_state=0)
 tic = time()
-y_pred_m1 = cross_val_predict(clf, X, y, cv=kfold)
+y_pred = cross_val_predict(clf, X, y, cv=kfold)
 toc = time()
-name_m1 = "Basic model"
-acc_m1, f1_m1, time_m1, cost_m1 = evaluate_model(y,
-                                                 y_pred_m1,
-                                                 name_m1,
-                                                 (toc-tic), cost_dict)
+appenders = evaluate_model(y, y_pred, "Basic model", (toc-tic), cost_dict)
+[lst.append(x) for lst, x in zip(lists, appenders)]
 
 # Simple random forest model after grouping QZ & LIM
 y_grouped = pd.Series(np.where(np.logical_or(y == "LIM", y == "QZ"), "LIM", y), name="litho_rock_class")
 clf = RandomForestClassifier(n_estimators=430, random_state=0)
 tic = time()
-y_pred_m2 = cross_val_predict(clf, X, y_grouped, cv=kfold)
+y_pred = cross_val_predict(clf, X, y_grouped, cv=kfold)
 toc = time()
-name_m2 = "Basic model after grouping QZ & LIM"
-acc_m2, f1_m2, time_m2, cost_m2 = evaluate_model(y_grouped,
-                                                 y_pred_m2,
-                                                 name_m2,
-                                                 (toc-tic), cost_dict)
+appenders = evaluate_model(y_grouped, y_pred,
+    "Basic model after grouping QZ & LIM", (toc-tic), cost_dict)
+[lst.append(x) for lst, x in zip(lists, appenders)]
 
 # Naive oversampled on simple random forest
-ros = RandomOverSampler(sampling_strategy='minority', random_state=123)
+ros = RandomOverSampler(random_state=123)
 clf = RandomForestClassifier(n_estimators=430, random_state=0)
 tic = time()
-y_pred_m3 = cros_val_predict_oversample(clf, X, y, ros, cv=kfold)
+y_pred = cros_val_predict_oversample(clf, X, y, ros, cv=kfold)
 toc = time()
-name_m3 = "Naive oversampled on simple random forest"
-acc_m3, f1_m3, time_m3, cost_m3 = evaluate_model(y,
-                                                 y_pred_m3,
-                                                 name_m3,
-                                                 (toc-tic), cost_dict)
+appenders = evaluate_model(y, y_pred,
+    "Naive oversampled on simple random forest", (toc-tic), cost_dict)
+[lst.append(x) for lst, x in zip(lists, appenders)]
 
-# # SMOTE oversampling on simple random forest
-# ros = SMOTE(sampling_strategy='minority', random_state=123)
-# clf = RandomForestClassifier(n_estimators=430, random_state=0)
-# tic = time()
-# y_pred_m4 = cros_val_predict_oversample(clf, X, y, ros, cv=kfold)
-# toc = time()
-# name_m4 = "SMOTE oversampling on simple random forest"
-# acc_m4, f1_m4, time_m4, cost_m4 = evaluate_model(y,
-#                                                  y_pred_m4,
-#                                                  name_m4,
-#                                                  (toc-tic), cost_dict)
-#
-# # Custom oversampling with 25 extra random samples on QZ
-# ros = FunctionSampler(func=custom_oversample,
-#                       kw_args={"random_state": 123,
-#                                "class_list": ["QZ"],
-#                                "num_samples": 25})
-# clf = RandomForestClassifier(n_estimators=430, random_state=0)
-# tic = time()
-# y_pred_m4 = cros_val_predict_oversample(clf, X, y, ros, cv=kfold)
-# toc = time()
-# name_m5 = "Custom oversampling with 25 extra random samples on QZ"
-# acc_m5, f1_m5, time_m5, cost_m5 = evaluate_model(y,
-#                                                  y_pred_m5,
-#                                                  name_m5,
-#                                                  (toc-tic), cost_dict)
+# SMOTE oversampling on simple random forest
+ros = SMOTE(sampling_strategy='minority', random_state=123)
+clf = RandomForestClassifier(n_estimators=430, random_state=0)
+tic = time()
+y_pred = cros_val_predict_oversample(clf, X, y, ros, cv=kfold)
+toc = time()
+appenders = evaluate_model(y, y_pred, "SMOTE oversampling on simple random forest", (toc-tic), cost_dict)
+[lst.append(x) for lst, x in zip(lists, appenders)]
+
+# Custom oversampling with 25 extra random samples on QZ
+ros = FunctionSampler(func=custom_oversample,
+                      kw_args={"random_state": 123,
+                               "class_list": ["QZ"],
+                               "num_samples": 25})
+clf = RandomForestClassifier(n_estimators=430, random_state=0)
+tic = time()
+y_pred = cros_val_predict_oversample(clf, X, y, ros, cv=kfold)
+toc = time()
+appenders = evaluate_model(y, y_pred,
+    "Custom oversampling with 25 extra random samples on QZ", (toc-tic), cost_dict)
+[lst.append(x) for lst, x in zip(lists, appenders)]
+
+# Custom oversampling with 50 extra random samples on QZ
+ros = FunctionSampler(func=custom_oversample,
+                      kw_args={"random_state": 123,
+                               "class_list": ["QZ"],
+                               "num_samples": 50})
+clf = RandomForestClassifier(n_estimators=430, random_state=0)
+tic = time()
+y_pred = cros_val_predict_oversample(clf, X, y, ros, cv=kfold)
+toc = time()
+appenders = evaluate_model(y, y_pred,
+    "Custom oversampling with 50 extra random samples on QZ", (toc-tic), cost_dict)
+[lst.append(x) for lst, x in zip(lists, appenders)]
+
+# Custom oversampling with 75 extra random samples on QZ
+ros = FunctionSampler(func=custom_oversample,
+                      kw_args={"random_state": 123,
+                               "class_list": ["QZ"],
+                               "num_samples": 75})
+clf = RandomForestClassifier(n_estimators=430, random_state=0)
+tic = time()
+y_pred = cros_val_predict_oversample(clf, X, y, ros, cv=kfold)
+toc = time()
+appenders =  evaluate_model(y, y_pred,
+    "Custom oversampling with 75 extra random samples on QZ", (toc-tic), cost_dict)
+[lst.append(x) for lst, x in zip(lists, appenders)]
+
+# Custom oversampling with 1 extra random samples on QZ
+ros = FunctionSampler(func=custom_oversample,
+                      kw_args={"random_state": 123,
+                               "class_list": ["QZ"],
+                               "num_samples": 1 })
+clf = RandomForestClassifier(n_estimators=430, random_state=0)
+tic = time()
+y_pred = cros_val_predict_oversample(clf, X, y, ros, cv=kfold)
+toc = time()
+appenders =  evaluate_model(y, y_pred,
+    "Custom oversampling with 1 extra random samples on QZ", (toc-tic), cost_dict)
+[lst.append(x) for lst, x in zip(lists, appenders)]
 
 # Simple random forest model after removing QZ
 y_no_qz = y.loc[y != "QZ"]
 X_no_qz = X.loc[y != "QZ"]
 clf = RandomForestClassifier(n_estimators=430, random_state=0)
 tic = time()
-y_pred_m6 = cross_val_predict(clf, X_no_qz, y_no_qz, cv=kfold)
+y_pred = cross_val_predict(clf, X_no_qz, y_no_qz, cv=kfold)
 toc = time()
-name_m6 = "Basic model after removing QZ"
-acc_m6, f1_m6, time_m6, cost_m6 = evaluate_model(y_no_qz,
-                                                 y_pred_m6,
-                                                 name_m6,
-                                                 (toc-tic), cost_dict)
+appenders = evaluate_model(y_no_qz, y_pred,
+    "Basic model after removing QZ", (toc-tic), cost_dict)
 
 df_summary = pd.DataFrame(data={
-    "Model Description": [name_m0, name_m1, name_m2, name_m3, name_m6],
-    "Accuracy": [acc_m0, acc_m1, acc_m2, acc_m3, acc_m6],
-    "Macro F1": [f1_m0, f1_m1, f1_m2, f1_m3, f1_m6],
-    "Evaluation Time": [time_m0, time_m1, time_m2, time_m3, time_m6],
-    "Absolute Explosive Diff": [cost_m0, cost_m1, cost_m2, cost_m3, cost_m6]
-})
+    "Model Description": names,
+    "Accuracy": accs,
+    "Macro F1": f1s,
+    "Evaluation Time": times,
+    "Absolute Explosive Diff": costs})
+
 df_summary.to_csv("doc/model_eval.csv")
--- pipeline/src/helpers/model.py
+++ pipeline/src/helpers/model.py
@@ -8,16 +8,16 @@ from collections import Counter
 
 def evaluate_model(y_true, y_pred, model_name, eval_time, cost_dict):
     unique_values = np.unique(y_true)
-    print("\n" + model_name + ":")
+    print("\n" + model_name.upper() + ":")
     print("Evaluated in %.2f s" % eval_time)
     confus = confusion_matrix(y_true, y_pred, labels=unique_values)
     #Correct headers
     confus_ex = pd.DataFrame(confus,
                    index=['true:'+x for x in unique_values],
                    columns=['pred:'+x for x in unique_values])
-    print('CONFUSION MATRIX:')
+    print('Confusion Matrix:')
     print(confus_ex)
-    print('CLASSIFICATION REPORT:')
+    print('Classification Report:')
     with warnings.catch_warnings():
         warnings.filterwarnings('ignore')
         f1 = f1_score(y_true, y_pred, average="macro")
@@ -28,7 +28,7 @@ def evaluate_model(y_true, y_pred, model_name, eval_time, cost_dict):
     acc = accuracy_score(y_true, y_pred)
 
 
-    return acc, f1, eval_time, overall_cost
+    return model_name, acc, f1, eval_time, overall_cost
 
 def calc_overall_cost(y_true, y_pred, cost_dict):
     if len(y_pred) != len(y_true):
@@ -42,7 +42,7 @@ def calc_overall_cost(y_true, y_pred, cost_dict):
     return diff_vector.sum()
 
 def cros_val_predict_oversample(estimator, X, y, oversampler, cv):
-    pred_list = []
+    pred_array = np.ndarray(len(y), dtype=object)
     for train_index, test_index in cv.split(X, y):
         X_train = X.loc[train_index]
         X_test = X.loc[test_index]
@@ -52,19 +52,18 @@ def cros_val_predict_oversample(estimator, X, y, oversampler, cv):
 
         #Train model in oversampled training set
         estimator.fit(X_train_res, y_train_res)
-        pred_values = estimator.predict(X_test).tolist()
-        pred_list.extend(pred_values)
+        pred_values = estimator.predict(X_test)
+        pred_array[test_index] = pred_values
 
-    return np.array(pred_list)
+    return pred_array
 
 def custom_oversample(X, y, class_list, num_samples, random_state=None):
-    index_vec = np.arange(stop=len(y))
+    index_vec = np.arange(len(y))
+
     for c in class_list:
-        resample_indeces = np.argwhere(y == c)
-        resample_indeces = np.random.choice(resample_indeces,
+        resample_indeces = np.random.RandomState(seed=random_state).choice(index_vec[y == c],
                                             size=num_samples,
-                                            replace=True,
-                                            random_state=random_state)
+                                            replace=True)
         index_vec = np.append(index_vec, resample_indeces)
 
-    return np.take(X, index_vec), np.take(y, index_vec)
+    return X[index_vec], y[index_vec]
